75db004c5b765bc26ea8b81da5c0f6139fbea083
mtomko <mjt0229@gmail.com>
2012-12-19 07:12:57 -0500

    typo

--- src/genetics/graph.clj
+++ src/genetics/graph.clj
@@ -1,3 +1,14 @@
 (ns genetics.graph
   (:require clojure.set))
 
+(def g {:a #{:b :c :d} :b {:e :c}})
+
+(defn add-node
+  [graph node]
+  (assoc graph node #{}))
+
+(defn add-directed-edge
+  [graph v0 v1]
+  (if (contains? graph v0)
+    (assoc graph (conj (v0 graph) v1))
+    (assoc graph v0 #{v1})))
