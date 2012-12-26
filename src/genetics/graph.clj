ea2a52873dce163cbae64de716802558538f5e22
mtomko <mjt0229@gmail.com>
2012-12-26 10:12:11 -0500

    Removed mutators from protocol; added weighted graph protocol; implemented directed graph

--- src/genetics/graph.clj
+++ src/genetics/graph.clj
@@ -3,13 +3,20 @@
 
 (defprotocol Graph
   (nodes [g] "Returns the nodes that comprise the graph")
-  (add-nodes [g nodes] "Adds the provided nodes to the graph")
+  (edges [g] "Returns the set of edges contained in the graph")
   (has-node? [g node] "Returns true iff the provided node is present in the graph" )
-  (edges [g] "Returns the set of edges contained in the graph" ))
+  (adjacent? [g n1 n2] "Returns true iff the provided nodes are adjacent in the graph")
+  (adjacencies [g node] "Returns the set of nodes adjacent to the provided node")
+  )
 
-(deftype DirectedGraph [nodes edges]
+(defprotocol WeightedGraph
+  (weight [n1 n2] "Returns the weight associated with the edge between the provided nodes"))
+
+(deftype DirectedGraph [adj]
   Graph
-    (nodes [this] nodes)
-    (add-nodes [this n] (update-in [:nodes] conj n))
-    (has-node? [this n] (contains? nodes n))
-    (edges [this] edges))
+    (nodes [this] (set (keys adj)))
+    (edges [this] adj)
+    (has-node? [this node] (contains? (nodes this) node))
+    (adjacent? [this n1 n2] (and (has-node? this n1)
+                              (contains? (adjacencies this n1) n2)))
+    (adjacencies [this node] (set (get adj node))))
