82bb00c61d7bd0f7e528d85aead72646ad118f4d
mtomko <mjt0229@gmail.com>
2012-12-30 21:24:50 -0500

    Cleanup code a bit

--- src/genetics/graph.clj
+++ src/genetics/graph.clj
@@ -16,27 +16,27 @@
   (weight [g n1 n2] "Returns the weight associated with the edge between the provided nodes"))
 
 ;; Default implementations
-(defn- nodes-
+(defn- def-nodes
   "Default implementation of nodes from Graph protocol"
   [adj]
   (key-set adj))
 
-(defn- edges-
+(defn- def-edges
   "Default implementation of edges from Graph protocol"
   [adj]
   adj)
 
-(defn- has-node?-
+(defn- def-has-node?
   "Default implementation of has-node? from Graph protocol"
   [nodes node]
   (contains? nodes node))
 
-(defn- adjacencies-
+(defn- def-adjacencies
   "Default implementation of adjacencies from Graph protocol"
   [adj node]
   (set (get adj node)))
 
-(defn- adjacent?-
+(defn- def-adjacent?
   "Default implementation of adjacent? from Graph protocol"
   [nodes adjacencies n1 n2]
   (and
@@ -46,19 +46,19 @@
 
 (deftype Graph [adj]
   IGraph
-    (nodes [this] (nodes- adj))
-    (edges [this] (edges- adj))
-    (adjacencies [this node] (adjacencies- adj node))
-    (has-node? [this node] (has-node?- (nodes this) node))
-    (adjacent? [this n1 n2] (adjacent?- (nodes this) adj n1 n2)))
+    (nodes [this] (def-nodes adj))
+    (edges [this] (def-edges adj))
+    (adjacencies [this node] (def-adjacencies adj node))
+    (has-node? [this node] (def-has-node? (nodes this) node))
+    (adjacent? [this n1 n2] (def-adjacent? (nodes this) adj n1 n2)))
 
 (deftype WeightedGraph [wadj]
   IGraph
-    (nodes [this] (nodes- wadj))
-    (edges [this] (edges- wadj))
+    (nodes [this] (def-nodes wadj))
+    (edges [this] (def-edges wadj))
     (adjacencies [this node] (key-set (get wadj node)))
-    (has-node? [this node] (has-node?- (nodes this) node))
-    (adjacent? [this n1 n2] (adjacent?- (nodes this) wadj n1 n2))
+    (has-node? [this node] (def-has-node? (nodes this) node))
+    (adjacent? [this n1 n2] (def-adjacent? (nodes this) wadj n1 n2))
   IWeightedGraph
     (weight [this n1 n2] (get-in wadj [n1 n2] Double/MAX_VALUE)))
 
@@ -73,31 +73,29 @@
   [n1 n2]
   (min-cmp (n1 2) (n2 2)))
 
-(defn- kruskal-
-  "Internal implementation of Kruskal's algorithm"
-  [nodes tree heap heap-insert heap-remove]
-  (cond (empty? heap) {} ;; in this case, no MST exists
-        (= nodes (key-set tree)) tree ;; in this case, we've completed the MST
-        :else
-          (let [[n1 n2 w] (heap-peek heap)]
-            (if (and (contains? tree n1) (contains? tree n2))
-              (recur nodes tree (heap-remove heap) heap-insert heap-remove)
-              ;; adds an edge in both directions, because we test for completion
-              ;; by looking at the nodes in t2 - not ideal, and should be cleaned up
-              (let [new-heap (heap-remove heap)
-                    new-tree (assoc-in (assoc-in tree [n1 n2] w) [n2 n1] w)]
-                (recur nodes new-tree new-heap heap-insert heap-remove))))))
-
 (defn- kruskal
   "Kruskal's algorithm for computing a minimum weight spanning tree for
   the graph."
-  [graph]
-  (let [[heap-insert heap-remove] (make-heap edge-cmp)
-        nodes (nodes graph)
+  ([graph]
+  ;; initialize the nodes, heap and MST, and then delegate
+  (let [nodes (nodes graph)
         edges (edge-seq (edges graph))
+        [heap-insert heap-remove] (make-heap edge-cmp)
         heap (reduce heap-insert [] edges)
         tree {}]
-      (kruskal- nodes tree heap heap-insert heap-remove)))
+      (kruskal nodes tree heap heap-insert heap-remove)))
+  ([nodes tree heap heap-insert heap-remove]
+    (cond (empty? heap) {}                ;; in this case, no MST exists
+          (= nodes (key-set tree)) tree   ;; in this case, we've completed the MST
+          :else                           ;; check the next smallest edge and continue
+            (let [[n1 n2 w] (heap-peek heap)]
+              (if (and (contains? tree n1) (contains? tree n2))
+                (recur nodes tree (heap-remove heap) heap-insert heap-remove)
+                ;; adds an edge in both directions, because we test for completion
+                ;; by looking at the nodes in t2 - not ideal, and should be cleaned up
+                (let [new-heap (heap-remove heap)
+                      new-tree (assoc-in (assoc-in tree [n1 n2] w) [n2 n1] w)]
+                  (recur nodes new-tree new-heap heap-insert heap-remove)))))))
 
 (defn minimum-spanning-tree
   "Computes a minimum weight spanning tree for the graph."
